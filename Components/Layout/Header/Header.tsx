a161d31606d44b93579603a0be8d1607e5b21ff6
cyrilodev <cyrildevelopper@gmail.com>
2022-01-09 19:09:49 +0100

    Commit Test - Delete

--- Components/Layout/Header/Header.tsx
+++ Components/Layout/Header/Header.tsx
@@ -3,7 +3,6 @@ import appTheme from "../../../styles/appTheme";
 import NavBar from "./NavBar";
 import ViridiosLogo from "./ViridiosLogo";
 
-/* Comment Test */
 const HeaderContainer = styled.header`
   display: flex;
   flex-direction: column;
